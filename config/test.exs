d52feb6732a05a376f9e94aa3bcd3dbba333b2c4
Marcin Zięba <nehhaket@gmail.com>
2023-06-02 22:47:23 +0200

    fix: test db config

--- config/test.exs
+++ config/test.exs
@@ -9,8 +9,7 @@ config :bcrypt_elixir, :log_rounds, 1
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :todo, Todo.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "marcin.zieba",
   hostname: "localhost",
   database: "todo_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
